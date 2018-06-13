4b5298d6cca428fae6f16ca21ee1f0142fa395e2
Austin Li <wagustin1207@yahoo.com>
2018-06-13 13:38:51 +0200

    started on trying to read in from STDIN, did case of it line is just a comment. Hoping the syntax isn't disgustingly wrong.

--- src/relabeler.go
+++ src/relabeler.go
@@ -3,7 +3,7 @@ package main
 import (
   "fmt"
   //"io"
-  //"bufio"
+  "bufio"
   //"os"
   "gopkg.in/alecthomas/kingpin.v2"
   )
@@ -15,7 +15,26 @@ import (
 func main() {
   //parse -label from command line
   kingpin.Parse()
+  //so now label is a "slice" (dynamic array kinda thing) containing
+  //all the desired custom labels (called by doing --label labelname)
 
   //read file from stdin
-  //scanner := bufio.NewScanner(os.Stdin)
+  scanner := bufio.NewScanner(os.Stdin)
+  var line String
+  line := scanner.Scan()
+
+  for line {
+    if scanner.Text()[0] == "#" {
+      fmt.Println("%s", scanner.Text())
+      line := scanner.Scan()
+    } else {
+      
+    }
+
+  }
+
+  //error catch block?
+  if err := scanner.Err(); err != nil {
+	   log.Println(err)
+  }
 }
