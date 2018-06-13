d4c4eb4357535f43a451df7c38f77e986bc96e85
Austin Li <wagustin1207@yahoo.com>
2018-06-13 12:49:29 +0200

    apparently I got the command-line label parsing right so here it is in case my laptop explodes

--- src/relabeler.go
+++ src/relabeler.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  //"io"
+  //"bufio"
+  //"os"
+  "gopkg.in/alecthomas/kingpin.v2"
+  )
+
+  var (
+    label = kingpin.Flag("label", "Add a custom label.").Strings()
+  )
 
 func main() {
-  fmt.Println("bla")
+  //parse -label from command line
+  kingpin.Parse()
+
+  //read file from stdin
+  //scanner := bufio.NewScanner(os.Stdin)
 }
