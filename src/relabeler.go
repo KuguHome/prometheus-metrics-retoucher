0f52535a4b8b6b499f52de47feee10a1726c96fd
Austin Li <wagustin1207@yahoo.com>
2018-06-14 15:14:34 +0200

    made it more pretty with comments *sparkle*

--- src/relabeler.go
+++ src/relabeler.go
@@ -3,8 +3,11 @@ package main
 import (
   "strings"
   "os"
+
   "gopkg.in/alecthomas/kingpin.v2"
+
   dto "github.com/prometheus/client_model/go"
+  
   "github.com/prometheus/common/expfmt"
   "github.com/golang/protobuf/proto"
   )
@@ -12,16 +15,21 @@ import (
   //set up the --label flag
   var (
     flagArgs = kingpin.Flag("label", "Add a label and value in
-      the form <name>=<value>.").Strings()
+      the form \"<label>=<value>\".").Strings()
   )
 
 func main() {
+  //parses command line flags
   kingpin.Parse()
 
+  //creates TextParser and parses text into metrics
   var parser expfmt.TextParser
   parsedFamilies, _ := parser.TextToMetricFamilies(os.Stdin)
 
+  //validPairs is a slice of POINTERS
   var validPairs []*dto.LabelPair
+
+  //parses the flag arguments. Ignores if not the correct format
   for _, elem := range *flagArgs {
     eqInd := strings.Index(elem, "=")
       if eqInd != -1 {
@@ -33,6 +41,7 @@ func main() {
       }
     }
 
+  //appends the valid pairs to the metrics and writes to StdOut
   for _, metricFamily := range parsedFamilies {
     for _, metric := range metricFamily.Metric {
       metric.Label = append(metric.Label, validPairs...)
