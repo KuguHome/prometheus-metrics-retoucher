043cdfec9fe8ed05d3a1f06fc82fa53ad8f0256f
Austin Li <wagustin1207@yahoo.com>
2018-06-14 14:25:55 +0200

    before changing a lot of stuff

--- src/relabeler.go
+++ src/relabeler.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-  //"fmt"
   "strings"
-  //"log"
-  //"io"
   "os"
   "gopkg.in/alecthomas/kingpin.v2"
   dto "github.com/prometheus/client_model/go"
@@ -17,32 +14,26 @@ import (
   )
 
 func main() {
-  //parse -label from command line
   kingpin.Parse()
-  //so now label is a "slice" (dynamic array kinda thing) containing
-  //all the desired custom labels (called by doing --label labelname)
-
-  //read file from stdin
-  //scanner := bufio.NewScanner(os.Stdin)
-  //var line bool
-  //line = scanner.Scan()
 
   var parser expfmt.TextParser
   parsedFamilies, _ := parser.TextToMetricFamilies(os.Stdin)
 
   for _, elem := range *str {
     eqInd := strings.Index(elem, "=")
+    pair := &dto.LabelPair{
+        Name:  proto.String(elem[:eqInd]),
+        Value: proto.String(elem[eqInd+1:]),
+      }
       if eqInd != -1 {
         for _, mf := range parsedFamilies {
           for _, m := range mf.Metric {
-            m.Label = append(m.Label, &dto.LabelPair{
-                Name:  proto.String(elem[:eqInd]),
-                Value: proto.String(elem[eqInd+1:]),
-            })
+            m.Label = append(m.Label, pair)
           }
         }
       }
     }
+
   for _, mf := range parsedFamilies {
     expfmt.MetricFamilyToText(os.Stdout, mf)
   }
