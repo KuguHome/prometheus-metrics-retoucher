4e68b4d272f54d5539b414a7cde080deac41ea9f
Austin Li <wagustin1207@yahoo.com>
2018-06-18 10:20:44 +0200

    got --in and --out working, I think. In the form --in infilename and --outfilename

--- src/relabeler.go
+++ src/relabeler.go
@@ -2,6 +2,8 @@ package main
 
 import (
   "os"
+  "io"
+  "bufio"
 
   "gopkg.in/alecthomas/kingpin.v2"
 
@@ -15,6 +17,8 @@ import (
   var (
     labelFlagArgs = kingpin.Flag("add-label", "Add a label and value in the form \"<label>=<value>\".").PlaceHolder("<label>=<value>").Short('a').StringMap()
     dropFlagArgs = kingpin.Flag("drop-metric", "Drop a metric").PlaceHolder("some_metric").Short('d').Strings()
+    inFileFlagArg = kingpin.Flag("in", "Accepts a file").File();
+    outFileFlagArg = kingpin.Flag("out", "Writes to a file").String(); //string because has to create the file
   )
 
 func main() {
@@ -23,7 +27,13 @@ func main() {
 
   //creates TextParser and parses text into metrics
   var parser expfmt.TextParser
-  parsedFamilies, _ := parser.TextToMetricFamilies(os.Stdin)
+  var reader io.Reader
+  if *inFileFlagArg == nil {
+    reader = os.Stdin
+  } else {
+    reader = bufio.NewReader(*inFileFlagArg)
+  }
+  parsedFamilies, _ := parser.TextToMetricFamilies(reader)
 
   //validPairs is a slice of POINTERS
   var validPairs []*dto.LabelPair
@@ -42,10 +52,26 @@ func main() {
   }
 
   //appends the valid pairs to the metrics and write everything to STDOUT
-  for _, metricFamily := range parsedFamilies {
-    for _, metric := range metricFamily.Metric {
-      metric.Label = append(metric.Label, validPairs...)
+  if *outFileFlagArg == "" {
+    for _, metricFamily := range parsedFamilies {
+      for _, metric := range metricFamily.Metric {
+        metric.Label = append(metric.Label, validPairs...)
+      }
+      expfmt.MetricFamilyToText(os.Stdout, metricFamily)
     }
-    expfmt.MetricFamilyToText(os.Stdout, metricFamily)
+  } else {
+      file, _ := os.Create(*outFileFlagArg)
+      for _, metricFamily := range parsedFamilies {
+        for _, metric := range metricFamily.Metric {
+          metric.Label = append(metric.Label, validPairs...)
+        }
+        expfmt.MetricFamilyToText(file, metricFamily)
+      }
   }
+
+  //this is here as a reminder that writing to STDOUT might need its own
+  //loop in case there's something else to do after appending valid pairs
+  //for _, metricFamily := range parsedFamilies {
+  //  expfmt.MetricFamilyToText(os.Stdout, metricFamily)
+  //}
 }
