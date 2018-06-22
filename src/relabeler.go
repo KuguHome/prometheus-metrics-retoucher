b1d1d04f29454bcf3985e60f89dcc2c7c6018466
Austin Li <wagustin1207@yahoo.com>
2018-06-22 15:31:24 +0200

    drops 38 metrics by default with drop-default-metrics

--- src/relabeler.go
+++ src/relabeler.go
@@ -19,6 +19,7 @@ import (
     dropFlagArgs = kingpin.Flag("drop-metric", "Drop a metric").PlaceHolder("some_metric").Short('d').Strings()
     inFileFlagArg = kingpin.Flag("in", "Read in a file").PlaceHolder("file_name").File();
     outFileFlagArg = kingpin.Flag("out", "Write to a File").PlaceHolder("file_name").String(); //string because has to create the file
+    defaultDropFlag = kingpin.Flag("drop-default-metrics", "Drop default metrics").Bool();
   )
 
 func main() {
@@ -45,6 +46,49 @@ func main() {
 					Value: proto.String(value),
 				})
       }
+
+  //add the default drop metrics to the list of metrics to be dropped
+  if *defaultDropFlag {
+    *dropFlagArgs = append(*dropFlagArgs, []string{
+      "go_memstats_last_gc_time_seconds",
+      "go_goroutines",
+      "go_memstats_other_sys_bytes",
+      "go_gc_duration_seconds",
+      "process_virtual_memory_bytes",
+      "go_memstats_heap_inuse_bytes",
+      "process_open_fds",
+      "go_memstats_heap_alloc_bytes",
+      "go_threads",
+      "go_memstats_mcache_inuse_bytes",
+      "process_max_fds",
+      "go_memstats_alloc_bytes",
+      "http_response_size_bytes",
+      "process_start_time_seconds",
+      "go_memstats_heap_released_bytes",
+      "go_memstats_sys_bytes",
+      "go_memstats_heap_idle_bytes",
+      "process_resident_memory_bytes",
+      "go_memstats_mcache_sys_bytes",
+      "go_memstats_frees_total",
+      "go_memstats_heap_objects",
+      "go_memstats_next_gc_bytes",
+      "go_memstats_buck_hash_sys_bytes",
+      "go_memstats_stack_sys_bytes",
+      "go_memstats_heap_sys_bytes",
+      "go_memstats_mspan_inuse_bytes",
+      "go_memstats_gc_cpu_fraction",
+      "go_memstats_stack_inuse_bytes",
+      "http_request_duration_microseconds",
+      "go_memstats_mspan_sys_bytes",
+      "go_info",
+      "go_memstats_gc_sys_bytes",
+      "http_requests_total",
+      "go_memstats_lookups_total",
+      "process_cpu_seconds_total",
+      "go_memstats_mallocs_total",
+      "go_memstats_alloc_bytes_total",
+      "http_request_size_bytes"}...)
+  }
 
   //delete metrics requested to be dropped
   for _, name := range *dropFlagArgs {
